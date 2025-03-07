c15bc72fdfd232b67a5a4ed729c833a5aa163ee7
szt3molcsa <szt3molcsa@vasvari.org>
2025-03-07 10:40:23 +0100

    middle of timetoable

--- idopontFoglalo/src/stores/timeTable.js
+++ idopontFoglalo/src/stores/timeTable.js
@@ -1,20 +1,45 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios';
 
 export const useTimeTableStore = defineStore('timeTable', () => {
+  const table = ref([])
   const filledSlots = ref([])
   const days = ref(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
   const startHour = ref(8)
   const endHour = ref(16)
+
+  const apiClient = axios.create({
+    baseURL: 'http://localhost:3000/foglaltIdopontok',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
   const FillSlot = (day, hour, name, phoneNumber)=>{
     //axios push
     return;
   }
   const FetchFilledSlots = ()=>{
-    //axios get
+    apiClient.get('/')
+  .then(response => {
+    filledSlots.value = response.data;
+  })
+  .catch(error => {
+    console.error(error);
+  });
   }
-  const IsSlotFilled = ()=>{
-    return false;
+  const TableGen = ()=>{
+    for (let index = 0; index < days.length; index++) {
+      const currentDay = array[index];
+      for (let currentHour = startHour; currentHour <= endHour; currentHour++) {
+        filledSlots.forEach(element => {
+          if(element.day == currentDay && element.hour == currentHour={})
+        });
+        
+      }
+      
+    }
   }
 
   return { filledSlots, days, FillSlot, FetchFilledSlots, IsSlotFilled }
--- idopontFoglalo/src/views/HomeView.vue
+++ idopontFoglalo/src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup>
-
+import { useTimeTableStore } from '@/stores/timeTable';
+import { ref, computed } from 'vue'
+const timeTable = useTimeTableStore()
+timeTable.FetchFilledSlots();
 </script>
 
 <template>
   <main>
-    <div v-for="">
-
+    <div v-for=" slotFilled in timeTable.filledSlots" >
+      <p>{{ slotFilled.name }}</p>
     </div>
   </main>
 </template>
